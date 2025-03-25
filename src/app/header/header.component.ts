ca5c76de4c07e05653363265d4dad7b33e873e65
AlvaroR <alvaro.ro02g@gmail.com>
2025-03-25 11:01:50 -0300

    Conexion con BD, LOGIN USUARIO Y REGISTER USUARIO(Carpeta server,services)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';  
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isMenuVisible = false;
+  sesionIniciada = false;
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.user$.subscribe(user => {
+      this.sesionIniciada = !!user;
+      console.log('Estado de sesión actualizado:', this.sesionIniciada);
+    });
+  }
 
   toggleMenu() {
-      this.isMenuVisible = !this.isMenuVisible;
+    this.isMenuVisible = !this.isMenuVisible;
   }
 
-  sesionIniciada = false;
-  opcionesUsuario(){
-    this.sesionIniciada = !this.sesionIniciada; //Por el momento es para probar como se ve la version con la sesion iniciada
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
-
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +12,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
   isErrorVisible = false;
 
-  ValidarUsuario(){
-       this.isErrorVisible = !this.isErrorVisible;
-      }
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      identifier: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9@._-]+$')
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ]
+    });
+  }
+
+  onSubmit(): void {
+    const { identifier, password } = this.loginForm.value;
+    console.log('Datos enviados:', { identifier, password });
+
+    this.http.post('http://localhost:3000/api/login', { identifier, password })
+      .pipe(
+        tap((response: any) => {
+          console.log('Login exitoso:', response);
+
+          if (response.user) {
+            this.authService.login(response.user); // Guardar usuario en AuthService
+            console.log('Usuario guardado en AuthService y sessionStorage:', response.user);
+            this.router.navigate(['/']);
+          } else {
+            console.error('No se recibió el objeto "user" en la respuesta');
+          }
+        }),
+        catchError((error) => {
+          console.error('Error en la petición:', error);
+          this.isErrorVisible = true;
+          return of(error);
+        })
+      )
+      .subscribe();
+  }
 }
