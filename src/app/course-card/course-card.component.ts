3babb36cc75be47d59c52826117339f78b2cb990
bauti <graujbautista@gmail.com>
2025-06-02 10:56:37 -0300

    Implementación del servicio de cursos y conexión con el backend

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,24 +1,41 @@
-import { Component, Input, OnChanges } from '@angular/core';
-import { Course, courseList } from './course.mock';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+//import { Course, courseList } from './course.mock';
+import { CourseService } from '../services/course.service';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.scss']
 })
-export class CourseCardComponent implements OnChanges{
+export class CourseCardComponent implements OnInit, OnChanges{
 
+  constructor(private courseService: CourseService) {}
+  
   @Input() order:string = 'default';
-  courseList: Course[] = courseList;
+
+  courseList: any[] = [];
+
+  ngOnInit(): void {
+    // Cargar los cursos desde el service -> desde el backend
+    this.courseService.getCourses().subscribe(
+      (data) => {
+        this.courseList = data;
+        console.log('Cursos obtenidos:', data);
+      },
+      (error) => {
+        console.error('Error al obtener los cursos:', error);
+      }
+    );
+  }
 
   ngOnChanges(): void {
     // Ordena la lista según el valor de `order`
     if (this.order === 'az') {
-      this.courseList.sort((a, b) => a.title.localeCompare(b.title));
+      this.courseList.sort((a, b) => a.nom_curso.localeCompare(b.nom_curso));
       console.log("Ordenado por A-Z")
     } else {
       // Orden por defecto (puedes personalizarlo)
-      this.courseList.sort((a,b) => a.id - b.id);
+      this.courseList.sort((a,b) => a.idcurso - b.idcurso);
       console.log("Ordenado por ID")
     }
   }
--- src/app/course-info/course-info.component.ts
+++ src/app/course-info/course-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Course, courseList } from '../course-card/course.mock';
+// import { Course, courseList } from '../course-card/course.mock';
+import { CourseService } from '../services/course.service';
 
 @Component({
   selector: 'app-course-info',
@@ -8,18 +9,36 @@ import { Course, courseList } from '../course-card/course.mock';
   styleUrls: ['./course-info.component.scss']
 })
 export class CourseInfoComponent implements OnInit{
-  course?: Course;
-  courseList: Course[] = courseList
+  course: any;
   loading: boolean = true;
   showModal: boolean = false;
 
-  constructor(private _route: ActivatedRoute  ) { }
+  constructor(private _route: ActivatedRoute, private courseService: CourseService) { }
   
   ngOnInit(): void {
+     // Obtener el ID del curso desde la URL
+    this._route.params.subscribe(params => {
+      const courseId = +params['id']; // Convierte el ID a número
+      this.getCourseInfo(courseId);
+    });
+  }
+    getCourseInfo(id: number): void {
+      this.courseService.getCourseById(id).subscribe(
+        (data) => {
+          this.course = data;
+          this.loading = false;
+          console.log('Información del curso:', data);
+        },
+        (error) => {
+          console.error('Error al obtener el curso:', error);
+          this.loading = false;
+        }
+      );
+    }
 
-    setTimeout(() => {
+  /*  setTimeout(() => {
       this._route.params.subscribe(params => {
-      this.course = this.courseList.find(course => course.id == params['id']);
+      this.course = this.courseList.find(course => course.idcurso == params['id']);
       this.loading = false; 
     });
     }, 1500);
@@ -33,5 +52,5 @@ export class CourseInfoComponent implements OnInit{
   }
   cancelModal(){
     this.showModal = false;
-  }
+  }*/
 }
