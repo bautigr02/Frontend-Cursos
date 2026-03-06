dc3adb1fdc7e2f18da1f8d25a6efc0c7833cf487
bautigr02 <bautistagrau@Bautista-NBook.local>
2026-03-06 17:05:59 -0300

    deploy url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 // Configuración para ambiente de producción
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend-url.com/api'  // Cambiar por la URL real de tu backend en producción
+  apiUrl: 'https://backend-cursos-production.up.railway.app/api'  // Cambiar por la URL real de tu backend en producción
 };
